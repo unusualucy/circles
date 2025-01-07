7d712b20eebbd1a101f126d6704b2abb0d254f15
Lucy <lucy@blim.org>
2025-01-07 21:08:46 +0000

    Add throwable.

--- game.py
+++ game.py
@@ -15,11 +15,12 @@ VIOLET = (150, 0, 150)
 SCREEN_SIZE = (1200, 800)
 
 class Circle:
-    def __init__(self, pos, radius, colour, speed):
+    def __init__(self, pos, radius, colour, speedx, speedy):
         self.pos = pos
         self.radius = radius
         self.colour = colour
-        self.speed = speed
+        self.speedx = speedx
+        self.speedy = speedy
 
 def draw_circles(screen, circles):
     for c in circles:
@@ -30,43 +31,69 @@ def is_in_circle(pos, circle):
     offsetY = circle.pos[1] - pos[1]
     return (offsetX * offsetX + offsetY * offsetY) < circle.radius * circle.radius 
 
-def update_circle_positions(circles, dragging):
+def update_circle_positions(circles, dragging, offset):
     for c in circles:
         if dragging == c:
             pos = pygame.mouse.get_pos()
             c.pos[0] = pos[0] + offset[0]
             c.pos[1] = pos[1] + offset[1]
-            c.speed = 0
+            c.speedx = 0
+            c.speedy = 0
         else:
-            c.pos[1] = c.pos[1] + c.speed
-            c.speed = c.speed + ACCEL
+            c.speedy = c.speedy + ACCEL
+            c.pos[0] = c.pos[0] + c.speedx
+            c.pos[1] = c.pos[1] + c.speedy
 
 def check_for_bounces(circles, dragging):
     for c in circles:
         if dragging != c:
-            if SCREEN_SIZE[1] - c.pos[1] < c.radius:
-                c.pos[1] = SCREEN_SIZE[1] - c.radius
-                c.speed = -c.speed
+            if SCREEN_SIZE[1] < c.pos[1] + c.radius:
+                #c.pos[1] = SCREEN_SIZE[1] - c.radius
+                c.speedy = -c.speedy 
+            if SCREEN_SIZE[0] < c.pos[0] + c.radius:
+                c.speedx = -c.speedx
+            if 0 > c.pos[0] - c.radius:
+                c.speedx = -c.speedx
+                
+            for o in circles:
+                if c != o and c != dragging:
+                    xdiff = c.pos[0] - o.pos[0]
+                    ydiff = c.pos[1] - o.pos[1]
+                    
+                    if 2 * radius * 2 * radius > xdiff * xdiff + ydiff *ydiff:
+                        # Figure out the collision
+                        # normal = pygame.math.Vector2(xdiff, ydiff)
+                        # normal.normalize()
+                        
+                        # perp = pygame.math.Vector2(normal.y, -normal.x)
+                        
+                        
+                        
+                        
+                        
+                
 
 running = True
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("Lucy's Game")
 
 circles = [
-    Circle([100, 400], 50, RED, 0),
-    Circle([200, 400], 50, ORANGE, 0),
-    Circle([300, 400], 50, YELLOW, 0),
-    Circle([400, 400], 50, GREEN, 0),
-    Circle([500, 400], 50, BLUE, 0),
-    Circle([600, 400], 50, INDIGO, 0),
-    Circle([700, 400], 50, VIOLET, 0)
+    Circle([100, 400], 50, RED, 0, 0),
+    Circle([200, 400], 50, ORANGE, 0, 0),
+    Circle([300, 400], 50, YELLOW, 0.1, 0),
+    Circle([400, 400], 50, GREEN, 0, 0),
+    Circle([500, 400], 50, BLUE, 0, 0),
+    Circle([600, 400], 50, INDIGO, 0, 0),
+    Circle([700, 400], 50, VIOLET, 0, 0)
 ]
 
 ACCEL = 0.001
+THROW_SPEED = 0.2
 
 # the circle we are dragging
 dragging = None
 offset = None
+dragpos = None
 
 while running:
     ev = pygame.event.get()
@@ -81,13 +108,22 @@ while running:
                     break
 
         if event.type == pygame.MOUSEBUTTONUP:
-            dragging = None
-            offset = None
+            if dragging:
+                endpos = pygame.mouse.get_pos()
+                dragging.speedx = (endpos[0] - dragpos[0]) * THROW_SPEED
+                dragging.speedy = (endpos[1] - dragpos[1]) * THROW_SPEED
+                # Resetting drag variables
+                dragging = None
+                offset = None
+                dragpos = None
 
         if event.type == pygame.QUIT:
             running = False
 
-    update_circle_positions(circles, dragging)
+    if dragging:
+        dragpos = pygame.mouse.get_pos()
+        
+    update_circle_positions(circles, dragging, offset)
 
     check_for_bounces(circles, dragging)
 
---
+++ list.py
@@ -0,0 +1,8 @@
+list = False
+
+def lucys_add(num1, num2):
+    return num1+num2
+    
+print(lucys_add(5, 10))   
+print(lucys_add(4, lucys_add(5, 23)))
+print(lucys_add(9, 243))
